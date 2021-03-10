65eade32ba92f2256684c3b40e6574d724c396cd
Sam Gould <ssgould@wpi.edu>
2021-03-10 12:27:48 -0500

    potentially minimax

--- Bomberman/group23/testcharacter.py
+++ Bomberman/group23/testcharacter.py
@@ -165,10 +165,9 @@ class TestCharacter(CharacterEntity):
             return heuristic
 
     def smart_monster_in_range(self, wrld, distance):
-        return 0
+	return self.random_monster_in_range(wrld, distance)
 
-    def minimax(self, wrld):
-        pass
+
 
     # based on pseudocode from class lecture
     def a_star(self, wrld):
@@ -254,73 +253,134 @@ class TestCharacter(CharacterEntity):
         return best
 
 
-#loopy things
-	current_x = x
-	current_y = y
-	    for add_x in [-1, 0, 1]:
-                for add_y in [-1, 0, 1]:
-                    
-                    if (0 <= current_x+add_x < wrld.width()) and\
-                            (0 <= current_y+add_y < wrld.height()) and\
-                            not (wrld.wall_at(current_x+add_x, current_y+add_y)):
-				#???
-				#self.min_value(wrld, alpha, beta, current_x, current_y, 1)
-                        
-
-#from connect-n
-    def go(self, brd):
-        """Search for the best move (choice of column for the token)"""
-        value = -math.inf
-        alpha = value
-        best_cols = []
-        # print("alpha: " + str(alpha) + ", beta: " + str(math.inf))
-        for col in brd.free_cols():
-            new_val = self.min_value(result(brd, col), alpha, math.inf, 1, col)
-            print("col: " + str(col) + ", value: " + str(new_val))
-            if new_val > value:
-                value = new_val
-                best_cols = [col]
-            if new_val == value:
-                best_cols.append(col)
-            alpha = max(alpha, value)
-        return best_cols[int((len(best_cols)-1)/2)]
-
-    def max_value(self, wrld, alpha, beta, x, y, depth):
-        utility = self.utility(wrld, x, y)
-        if utility is not None:
-            return utility
-        value = -math.inf
-	# something about moving differently        
-	for next_col in brd.free_cols():
-            value = max(value, self.min_value(result(brd, next_col), alpha, beta, depth + 1, col))
-            if value > beta:
-                return value
-            alpha = max(alpha, value)
-        return value
-
-    def min_value(self, brd, alpha, beta, x, y, depth):
-        utility = self.utility(wrld, x, y)
-        if utility is not None:
-            return utility
-        value = math.inf
-	# something about moving differently
-        for next_col in brd.free_cols():
-            value = min(value, self.max_value(result(brd, next_col), alpha, beta, depth + 1, col))
-            if value < alpha:
-                # print(" " * depth + "value: " + str(value))
-                return value
-            beta = min(beta, value)
-        return value
 
 
-    def utility(self, wrld, x, y, depth):
-	exit = wrld.exitcell
-	if (x == exit[0]) && (x == exit[0])
-	    return wrld.time
-	elif game_end(wrld):
-	    return -100;
-	elif depth >= 3:
-	    return -max(abs(exit[0]-x), abs(exit[1]-y)) # not the proper dist formula
-	else
-	    return None
+
+def minimax(self, toClone):
+        wrld = SensedWorld.from_world(toClone)
+        cMoves = []
+        c = next(iter(wrld.characters.values()))[0]
+        # Loop through delta x
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bound indexing
+            if (c.x + dx >= 0) and (c.x + dx < wrld.width()):
+                # Loop through delta y
+                for dy in [-1, 0, 1]:
+                    # Avoid out-of-bound indexing
+                    if (c.y + dy >= 0) and (c.y + dy < wrld.height()):
+                        # No need to check impossible moves
+                        if not wrld.wall_at(c.x + dx, c.y + dy):
+                            # Set move in wrld
+                            c.move(dx, dy)
+                            # Get new world
+                            (newwrld, events) = wrld.next()
+                            b = 0
+                            for e in events:
+                                if (e.tpe == Event.CHARACTER_KILLED_BY_MONSTER):
+                                    b = 1
+                            if (b):
+                                continue
+                            value = self.minHelper(newwrld)
+                            moveTuple = (value, dx, dy)
+                            cMoves.append(moveTuple)
+        pMax = -800
+        i = 0
+        index = 0
+        for p in cMoves:
+            if (p[0] > pMax):
+                pMax = p[0]
+                index = i
+            i += 1
+        self.move(cMoves[index][1], cMoves[index][2])
+
+    def minHelper(self, wrld):
+        m = next(iter(wrld.monsters.values()))[0]
+        c = next(iter(wrld.characters.values()))[0]
+        heuristic = 0
+	distTuples = []
+        # Loop through delta x
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bound indexing
+            if (m.x + dx >= 0) and (m.x + dx < wrld.width()):
+                # Loop through delta y
+                for dy in [-1, 0, 1]:
+                    # Make sure the monster is moving
+                    if (dx != 0) or (dy != 0):
+                        # Avoid out-of-bound indexing
+                        if (m.y + dy >= 0) and (m.y + dy < wrld.height()):
+                            # No need to check impossible moves
+                            if not wrld.wall_at(m.x + dx, m.y + dy):
+                                # Set move in wrld
+                                m.move(dx, dy)
+                                # Get new world
+                                (newwrld, events) = wrld.next()
+                                for e in events:
+                                    if (e.tpe == Event.CHARACTER_KILLED_BY_MONSTER):
+                                        heuristic -= ((wrld.height() + wrld.width()) / 2)
+                                    elif (e.tpe == Event.CHARACTER_FOUND_EXIT):
+                                        heuristic += (wrld.height() + wrld.width())
+                                mm = next(iter(newwrld.monsters.values()))[0]
+                                difXcm = abs(c.x - mm.x)
+                                difYcm = abs(c.y - mm.y)
+                                difcm = sqrt((difXcm * difXcm) + (difYcm * difYcm))
+                                dist = (difcm, x, y)
+ 				distTuples.append(dist)
+
+	pMin = 800
+        i = 0
+        index = 0
+        for p in distTuples:
+            if (p[0] < pMin):
+                pMax = p[0]
+                index = i
+            i += 1
+        m.move(distTuple[1] - m.x, distTuple[2] - m.y)
+            return maxHelper(wrld)
+
+
+def maxHelper(self, wrld):
+        m = next(iter(wrld.monsters.values()))[0]
+        c = next(iter(wrld.characters.values()))[0]
+        heuristic = 0
+	hTuples = []
+        # Loop through delta x
+        for dx in [-1, 0, 1]:
+            # Avoid out-of-bound indexing
+            if (c.x + dx >= 0) and (c.x + dx < wrld.width()):
+                # Loop through delta y
+                for dy in [-1, 0, 1]:
+                    # Make sure the monster is moving
+                    if (dx != 0) or (dy != 0):
+                        # Avoid out-of-bound indexing
+                        if (c.y + dy >= 0) and (c.y + dy < wrld.height()):
+                            # No need to check impossible moves
+                            if not wrld.wall_at(c.x + dx, c.y + dy):
+                                # Set move in wrld
+                                c.move(dx, dy)
+                                # Get new world
+                                (newwrld, events) = wrld.next()
+                                for e in events:
+                                    if (e.tpe == Event.CHARACTER_KILLED_BY_MONSTER):
+                                        heuristic -= ((wrld.height() + wrld.width()) / 2)
+                                    elif (e.tpe == Event.CHARACTER_FOUND_EXIT):
+                                        heuristic += (wrld.height() + wrld.width())
+                                cc = next(iter(newwrld.characters.values()))[0]
+                                difXcm = abs(cc.x - m.x)
+                                difYcm = abs(cc.y - m.y)
+                                difcm = sqrt((difXcm * difXcm) + (difYcm * difYcm))
+                                dist = (difcm, x, y)
+				hueristic = dist - max(abs(exit[0]-x), abs(exit[1]-y))
+				h = (heuristic, x, y)
+ 				hTuples.append(dist)
+
+	pMax = -800
+        i = 0
+        index = 0
+        for p in hTuples:
+            if (p[0] > pMax):
+                pMax = p[0]
+                index = i
+            i += 1
+            return pMax
+
 
